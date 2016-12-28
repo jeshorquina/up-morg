052d16813d8c46b6abbfdb480f66e7182f5e5418
Jeshurun Orquina <jeshorquina@gmail.com>
2016-12-28 20:41:25 +0800

    Add Database Schema

---
+++ database/upmorg/Addition to Schema/003_CreateMemberTypeTable.sql
@@ -0,0 +1,7 @@
+USE upmorg;
+
+CREATE TABLE IF NOT EXISTS `MemberType` (
+    `MemberTypeID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `MemberType` INT NOT NULL,
+    PRIMARY KEY(`MemberTypeID`)
+);
