fd082c4c195e3e4550c0ba7e2bf4ae23d6e285c3
Jeshurun Orquina <jeshorquina@gmail.com>
2017-01-08 03:03:38 +0800

    Signup Validation

--- application/operations/UserActionOperations.php
+++ application/operations/UserActionOperations.php
@@ -21,4 +21,29 @@ class UserActionOperations {
     {   
         return self::$repository->GetPassword($username) === $password;
     }
+    
+    public function IsRegistrationDataValid($first_name, $middle_name, $last_name, 
+                                            $email_address, $phone_number, 
+                                            $first_password, $second_password)
+    { 
+        $first_name         = filter_var($first_name, FILTER_SANITIZE_STRING);
+        $middle_name        = filter_var($middle_name, FILTER_SANITIZE_STRING);
+        $last_name          = filter_var($last_name, FILTER_SANITIZE_STRING);
+        $email_address      = filter_var($email_address, FILTER_SANITIZE_EMAIL);
+        $phone_number       = filter_var($phone_number, FILTER_SANITIZE_NUMBER_INT);
+        $first_password     = filter_var($first_password, FILTER_SANITIZE_STRING);
+        $second_password    = filter_var($second_password, FILTER_SANITIZE_STRING);
+
+        if(!filter_var($email_address, FILTER_VALIDATE_EMAIL))
+        {
+            return false;
+        }
+
+        if($first_password !== $second_password)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
